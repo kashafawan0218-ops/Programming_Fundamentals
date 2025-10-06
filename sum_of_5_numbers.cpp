0f4047b8b1e6ec2623d71aa46e74364cce7b6f1e
kashafawan0218-ops <kashafawan0218@gmail.com>
2025-10-06 20:15:32 +0500

    Add files via upload
    Through this code you can give 5 numbers and can get their sum.

---
+++ sum_of_5_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Please enter your 5 numbers"<<endl;
+	int a,b,c,d,e;
+	cin>>a>>b>>c>>d>>e;
+	int sum=a+b+c+d+e;
+	cout<<"The sum is "<<sum<<endl;
+	return 0;
+}
